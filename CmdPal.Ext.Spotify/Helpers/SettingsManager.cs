949d4297ef5e0b1b9de1472d708b12f0a53194e5
Fiatsoft <github.oss@fiatsoft.com>
2025-08-04 03:33:55 -0400

    with [Hide]/KeepOpen/GoHome result-setting per PlayerCommand (by internal static SpotifyCommandsProvider.SettingsManager), track-list helper command exclusions by Type-list

--- CmdPal.Ext.Spotify/Helpers/SettingsManager.cs
+++ CmdPal.Ext.Spotify/Helpers/SettingsManager.cs
@@ -1,6 +1,11 @@
-﻿using CmdPal.Ext.Spotify.Properties;
+﻿using CmdPal.Ext.Spotify.Commands;
+using CmdPal.Ext.Spotify.Properties;
 using Microsoft.CommandPalette.Extensions.Toolkit;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace CmdPal.Ext.Spotify.Helpers;
 
@@ -36,12 +41,43 @@ public class SettingsManager : JsonSettingsManager
 
     public string FilterWildcard => _filterWildcard.Value;
 
+    public Dictionary<Type, ChoiceSetSetting> CommandResults { get; } = new();
+
     public SettingsManager()
     {
         FilePath = SettingsJsonPath();
 
         Settings.Add(_clientId);
         Settings.Add(_filterWildcard);
+
+        var choices = new List<ChoiceSetSetting.Choice>() {
+            new ChoiceSetSetting.Choice(Resources.ExtensionSettingCommandResultActionHide, "Hide"),
+            new ChoiceSetSetting.Choice(Resources.ExtensionSettingCommandResultActionKeepOpen, "KeepOpen"),
+            new ChoiceSetSetting.Choice(Resources.ExtensionSettingCommandResultActionGoHome, "GoHome")
+        };
+        foreach (var type in new Type[] {
+            typeof(AddToQueueCommand),
+            typeof(LoginCommand),
+            typeof(PausePlaybackCommand),
+            typeof(ResumePlaybackCommand),
+            typeof(SetRepeatCommand),
+            typeof(SetShuffleCommand),
+            typeof(SkipNextCommand),
+            typeof(SkipPreviousCommand),
+            typeof(TogglePlaybackCommand),
+            typeof(TransferPlaybackCommand)
+        })
+        {
+            CommandResults.Add(type, new ChoiceSetSetting(
+                    key: type.Name,
+                    label: string.Format(Resources.ExtensionSettingCommandResultSettingLabel, Resources.ResourceManager.GetString(type.Name)),
+                    description: string.Format(Resources.ExtensionSettingCommandResultSettingDesc, type.Name),
+                    choices: choices
+                )
+            );
+        }
+        foreach (var choiceSetSetting in CommandResults.Values)
+            Settings.Add(choiceSetSetting);
 
         LoadSettings();
 
